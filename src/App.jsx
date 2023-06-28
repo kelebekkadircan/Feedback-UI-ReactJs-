ad941c52ed536b6b7e234d2758dda8473edadb9a
kelebekkadircan <kelebekkadircan@gmail.com>
2023-06-28 11:21:16 +0300

    Feedback UI ReactJs

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
+import { v4 as uuidv4 } from 'uuid'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Header from "./components/Header";
-import FeedbackItem from "./components/FeedbackItem";
+// import FeedbackItem from "./components/FeedbackItem";
 import { useState } from "react";
 import FeedbackList from "./components/FeedbackList";
-import FeedbackData from "./data/FeedbackData";
 import Card from "./components/shared/Card";
 import FeedbackStats from "./components/FeedbackStats";
+import FeedbackForm from "./components/FeedbackForm";
+import FeedbackData from "./data/FeedbackData";
+import AboutIconLink from './components/AboutIconLink';
+import AboutPage from './pages/AboutPage';
 
 
 function App() {
 
     const [feedback, setFeedback] = useState(FeedbackData)
+
+    const addFeedback = (newFeedback) => {
+        newFeedback.id = uuidv4()
+        setFeedback([newFeedback, ...feedback])
+        console.log(newFeedback);
+    }
 
     const deleteFeedback = (id) => {
         if (window.confirm('are you sure you want to delete')) {
@@ -20,18 +31,29 @@ function App() {
 
     return (
 
-        <>
+        <Router>
             <Header />
             <div className="container">
                 {/* <h1>My App</h1> */}
-                <FeedbackStats feedback={feedback} />
-                <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
-                <Card>
-                    Hello World
-                </Card>
+                <Routes>
+                    <Route exact path='/' element={
+                        <>
+                            <FeedbackForm handleAdd={addFeedback} />
+                            <FeedbackStats feedback={feedback} />
+                            <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+                        </>
+                    }>
+
+                    </Route>
+
+                    <Route path='/about' element={<AboutPage />} />
+                </Routes>
+                <AboutIconLink />
+
             </div>
 
-        </>
+
+        </Router>
     )
 }
 export default App
